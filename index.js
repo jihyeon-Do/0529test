9053d372ff7e58e31ee07131cefe21e44290a179
domuk-k <dannyworks102@gmail.com>
2020-05-29 11:44:28 +0900

    feat: view completed

--- index.js
+++ index.js
@@ -1,31 +1,32 @@
 const todos = [{
-    id: 3,
-    content: 'HTML',
-    completed: false
-  },
-  {
-    id: 2,
-    content: 'CSS',
-    completed: true
-  },
-  {
-    id: 1,
-    content: 'Javascript',
-    completed: false
-  }
+  id: 3,
+  content: 'HTML',
+  completed: false
+},
+{
+  id: 2,
+  content: 'CSS',
+  completed: true
+},
+{
+  id: 1,
+  content: 'Javascript',
+  completed: false
+}
 ];
 
 function render() {
-  let html = '';
+  const test = document.querySelector('.test')
 
-  todos.forEach(todo => {
-
+  todos.forEach(({ id, content, completed }) => {
+    test.innerHTML += `<li id=${id}><label><input type="checkbox"${completed ? ' checked' : ''}>${content}</label></li>`
   });
 
-  return html;
+  // return test;
 }
+render()
+//console.log(render());
 
-console.log(render());
 /*
 <li id="3">
   <label><input type="checkbox">HTML</label>
